019266ec02df689f23005533bf5de3fa1826f313
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-03-05 12:02:02 +0100

    Create class Message

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
 const inquirer = require('inquirer');
 
-const startApp = () => {
-  inquirer.prompt([{
-    name: 'action',
-    type: 'input',
-    message: 'How can I help you?',
-  }]).then((answers: { action: 'list' | 'add' | 'remove' | 'quit' }) => {
-    console.log("Chosen action: " + answers.action);
-    startApp();
-    if (answers.action === "quit")
-      return;
-  });
+import Message from './Message';
+import { ColorOption } from './Message'
+
+
+enum Action {
+    List = "list",
+    Add = "add",
+    Remove = "remove",
+    Quit = "quit"
 }
 
-startApp();
+type InquirerAnswers = {
+    action: Action
+}
+
+
+const startApp = () => {
+    inquirer.prompt([{
+        name: 'action',
+        type: 'input',
+        message: 'How can I help you?',
+    }]).then(async (answers: InquirerAnswers) => {
+        console.log("Chosen action: " + answers.action);
+        startApp();
+        if (answers.action === "quit")
+            return;
+    });
+}
+
+// startApp();
